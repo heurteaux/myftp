80ad82ea5a36fd4a0cded07071cf7644d536aa14
Colin Heurteaux <accounts@heurteaux.net>
2025-02-27 09:15:58 +0100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2025
+** nanotekspice
+** File description:
+** main.cpp
+*/
+
+#include <iostream>
+#include <memory>
+
+int main()
+{
+    std::cout << "Hello, world !" << std::endl;
+    return 0;
+}
